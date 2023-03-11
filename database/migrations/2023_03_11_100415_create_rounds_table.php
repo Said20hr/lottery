01b7b3bd2a147a2c08b901a2f3ac8514b4e01a7e
SAID HADJ RAMDANE <saidhr20@gmail.com>
2023-03-11 11:13:18 +0100

    first commit

---
+++ database/migrations/2023_03_11_100415_create_rounds_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rounds', function (Blueprint $table) {
+            $table->id();
+            $table->integer('draw_number');
+            $table->json('blue_numbers');
+            $table->json('yellow_numbers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rounds');
+    }
+};
